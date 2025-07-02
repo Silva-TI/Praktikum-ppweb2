4f98fd8db003acfa910ce86620e6df1178c79c58
Silva Nurzanatul Dahmalena <0110224021@student.nurulfikri.ac.id>
2025-07-02 11:32:48 +0700

    Update form_nilai.php

--- Praktikum_02/form_nilai.php
+++ Praktikum_02/form_nilai.php
@@ -75,7 +75,7 @@
     <footer class="mt-5" style="bottom: 0; left: 0; right: 0; position: fixed;">
         <nav class="navbar navbar-expand-md navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">Develop By @aiiiiilaaaa &copy;<?= date("Y") ?></a>
+                <a class="navbar-brand" href="#">Develop By @yujingf &copy;<?= date("Y") ?></a>
             </div>
         </nav>
     </footer>
